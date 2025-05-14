88ac13420f0fec79747cde720cb76066f7aff20f
Cheungyaa <netjjang990126@gmail.com>
2025-05-14 14:00:44 +0900

    어쩔티비 추가

--- src/Bridge.java
+++ src/Bridge.java
@@ -13,6 +13,11 @@ class LGTV implements TV {
     public void off() { System.out.println("LG TV OFF"); }
 }
 
+class HowTV implements TV {
+    public void on() { System.out.println("How TV ON"); }
+    public void off() { System.out.println("How TV OFF"); }
+}
+
 abstract class RemoteControl {
     protected TV tv;
     public RemoteControl(TV tv) { this.tv = tv; }
@@ -28,8 +33,16 @@ class BasicRemote extends RemoteControl {
 
 public class Bridge {
     public static void main(String[] args) {
-        RemoteControl remote = new BasicRemote(new SamsungTV());
-        remote.turnOn();
-        remote.turnOff();
+        RemoteControl remote1 = new BasicRemote(new SamsungTV());
+        remote1.turnOn();
+        remote1.turnOff();
+
+        RemoteControl remote2 = new BasicRemote(new LGTV());
+        remote2.turnOn();
+        remote2.turnOff();
+
+        RemoteControl remote3 = new BasicRemote(new HowTV());
+        remote3.turnOn();
+        remote3.turnOff();
     }
 }
